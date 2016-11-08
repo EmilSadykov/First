3f5951f53f435856d325b485ef3734fe329711e5
EmilSadykov <emillion2010@mail.ru>
2016-11-08 09:39:54 +0300

    Update 1

--- Student.java
+++ Student.java
@@ -1,16 +1,29 @@
 public class Student {
-
+    private String DEFAULT_NAME = "DEFAULT";
+    private int DEFAULT_MAX_COUNT = 10;
     private String name;
     private int age;
     private int[] marks;
     private double middleMark;
-
+    
+    //пустой конструктор
+    public Student() {
+        this.name = DEFAULT_NAME;
+        this.marks= int[DEFAULT_MAX_COUNT];
+    }
+ 
+    //конструктор с параметрами
     public Student(String name, int age, int[] marks) {
         this.name = name;
         this.age = age;
-        this.marks = marks;
+        // this.marks = marks; - передается ссылка
+        this.marks = new int[marks.length];
+        for (int i = 0; i < marks.length; i++) {
+            this.marks[i] = marks[i];
+        }
         setMiddleMark(marks);
     }
+
     private void setMiddleMark(int[] marks) {
         double sum = 0;
         int marksCount = 0;
@@ -30,25 +43,32 @@ public class Student {
             this.age = 0;
         }
     }
+    
     public int getAge() {
         return this.age;
     }
+    
     public void setName(String name) {
         this.name = name;
     }
+    
     public String getName() {
         return this.name;
     }
+    
     public void setMarks(int[] marks) {
         this.marks = marks;
         setMiddleMark(marks);
     }
+    
     public int[] getMarks() {
         return this.marks;
     }
+    
     public void setMiddleMark() {
 
     }
+    
     public double getMiddleMark() {
         return this.middleMark;
     }
