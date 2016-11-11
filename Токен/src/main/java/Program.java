180f14a65e99503bd62a0984bcaf45e93b74e987
EmilSadykov <emillion2010@mail.ru>
2016-11-11 22:20:58 +0300

    gitignore

--- Токен/src/main/java/Program.java
+++ Токен/src/main/java/Program.java
@@ -5,9 +5,9 @@ public class Program {
     public static void main(String[] args) {
         NativeTokenizerImpl nativeTokenizer = new NativeTokenizerImpl();
         Token[] token = new Token[0];
-        token = nativeTokenizer.tokenize("Привет - word, 0, 6, Привет");
+        token = nativeTokenizer.tokenize("123 - number, 9, 11, 123");
         for (int i = 0; i < token.length; i++) {
-            System.out.println(token[i].text + " : " + token[i].text + " : " + token[i].text);
+            System.out.println(token[i].begin + " : " + token[i].end + " : " + token[i].text);
         }
     }
 }
